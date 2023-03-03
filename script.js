fd3730bf1e80bb5113c6475361f617e925aeaf8d
Kieran <114930899+KizzaD91@users.noreply.github.com>
2023-03-03 15:00:26 +0000

    Update icons and code mobile menu

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 // Secret Fact:
 
-const secretFact = document.getElementById('secret-fact');
+const secretFact = document.getElementById("secret-fact");
 
-secretFact.addEventListener('click', () => {
-    secretFact.innerHTML === "Click here for a secret fact..." ? secretFact.innerHTML = "✔️ I love classical music!  Click me to hide me again!" : secretFact.innerHTML = "Click here for a secret fact...";
+secretFact.addEventListener("click", () => {
+  secretFact.innerHTML === "Click here for a secret fact..."
+    ? (secretFact.innerHTML =
+        "✔️ I love classical music!  Click me to hide me again!")
+    : (secretFact.innerHTML = "Click here for a secret fact...");
 
-    secretFact.style.fontWeight === '400' ? secretFact.style.fontWeight = '700' : secretFact.style.fontWeight = '400';
-    secretFact.style.color === 'black' ? secretFact.style.color = 'crimson' : secretFact.style.color = 'black';
-})
+  secretFact.style.fontWeight === "400"
+    ? (secretFact.style.fontWeight = "700")
+    : (secretFact.style.fontWeight = "400");
+  secretFact.style.color === "black"
+    ? (secretFact.style.color = "crimson")
+    : (secretFact.style.color = "black");
+});
 
 // Date for footer
 
 let date = new Date();
 let year = date.getFullYear();
 
-const dateText = document.getElementById('current-date');
+const dateText = document.getElementById("current-date");
 dateText.innerHTML = year;
+
+// Menu functionality
+
+// Variable for menu icon
+
+const menuIcon = document.getElementById("menu-icon");
+const mobileMenu = document.getElementById('mobile-nav');
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.innerHTML === 'menu' ? menuIcon.innerHTML = 'close' : menuIcon.innerHTML = 'menu';
+  mobileMenu.style.display === 'flex' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = 'flex';
+});
